1bf61cdbf539e713355bbdd756eb85e7b75e7b8a
lucky zong <905426593@qq.com>
2018-03-14 10:37:02 +0800

    first add

---
+++ Desktop/iOS知识点/UIView/UIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView
+//
+//  Created by lucky zong on 2018/3/14.
+//  Copyright © 2018年 lingqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desktop/iOS知识点/UIView/UIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView
+//
+//  Created by lucky zong on 2018/3/14.
+//  Copyright © 2018年 lingqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
